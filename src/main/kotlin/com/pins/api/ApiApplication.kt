8f7ba95ad56d30df82c1d9836319db67deb1a986
Brian Elias <elibrianga@gmail.com>
2021-10-21 16:47:34 +0300

    Reset Project TODO solve CSRF problem

--- src/main/kotlin/com/pins/api/ApiApplication.kt
+++ src/main/kotlin/com/pins/api/ApiApplication.kt
@@ -2,8 +2,10 @@ package com.pins.api
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories
 
 @SpringBootApplication
+@EnableNeo4jRepositories
 class ApiApplication
 
 fun main(args: Array<String>) {
