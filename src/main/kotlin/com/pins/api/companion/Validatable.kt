567bd50600b2b94b4a2e04930c1204b5f74e83e1
Brian Elias <elibrianga@gmail.com>
2021-11-15 17:53:50 +0300

    >> OWNER Role

--- src/main/kotlin/com/pins/api/companion/Validatable.kt
+++ src/main/kotlin/com/pins/api/companion/Validatable.kt
@@ -1,12 +1,12 @@
 package com.pins.api.companion
 
-import jdk.nashorn.internal.ir.annotations.Ignore
+import com.fasterxml.jackson.annotation.JsonIgnore
 
 /**
  * Requires implementation of a 'valid' function which return a boolean
  * This interface offers a streamlined way to check if an object is valid
  */
 interface Validatable {
-    @Ignore
+    @JsonIgnore
     abstract fun valid() : Boolean
 }
