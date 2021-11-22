55a46eef27764e64b2b0f356fcf1d2fc2470b2f4
Brian Elias <elibrianga@gmail.com>
2021-11-22 15:12:42 +0300

    >> Posts related to location functions

--- src/main/kotlin/com/pins/api/repository/PostRepository.kt
+++ src/main/kotlin/com/pins/api/repository/PostRepository.kt
@@ -2,9 +2,16 @@ package com.pins.api.repository
 
 import com.pins.api.entities.content.Post
 import org.springframework.data.neo4j.repository.Neo4jRepository
+import org.springframework.data.neo4j.repository.query.Query
 
 interface PostRepository : Neo4jRepository<Post,Long> {
 
+    // match (l:Location)-[*]-(l2:Location)-[:LOCATIONS]-(pl:PostLocations)-[:PINNED_AT]-(p:Post) where l.lat = 23 and l.lon = 25 return p
+    // get post related to location
+    @Query("MATCH (l:Location)-[*]-(l2:Location)-[:LOCATIONS]-(pl:PostLocations)-[:PINNED_AT]-(p:Post) WHERE l.lat = \$lat AND l.lon = \$lon RETURN p")
+    fun getPostRelatedToLocation(lat:Double, lon : Double):List<Post>
 
+    @Query("MATCH (l:Location)-[*]-(l2:Location)-[:LOCATIONS]-(pl:PostLocations)-[:PINNED_AT]-(p:Post) WHERE l.lat = \$lat AND l.lon = \$lon RETURN count(p)")
+    fun numberOfPostRelatedToLocation(lat:Double, lon : Double) : Double
 
 }
